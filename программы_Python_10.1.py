cfb3fed1cb7c57c2cb2d73e7c1dd2b079f1b712d
angbella <71321858+angbella@users.noreply.github.com>
2020-11-03 21:09:28 +0700

    Update программы_Python_10.1.py

--- программы_Python_10.1.py
+++ программы_Python_10.1.py
@@ -1,6 +1,26 @@
+B.
 from math import *
 def x(a):
     return (sqrt(a)+a))
 def x2(b):
     return (sqrt(b)+b))
 print(x1(5)/x2(7)+x1(12)/x2(8)+x1(31)/x2(2))
+c.
+def summa (a,b)
+    s1=a+sqrt (b)
+    return s1
+def summa(c,d)
+    s2=c+sqrt (d)
+    return s2
+print (s1(15,8)/s2(8,15)+s1(6,12)+s2(12,6)+s1(7,21)/s2(21,7))
+D.
+def summa (a,b)
+    s1= (a+sqrt(b))/(sqrt (b)+a)
+    return s1
+def summa (c,d)
+    s2=(c+sqrt(d))/(sqrt(c)+d)
+    return s2
+def summa (f,g)
+    s3=(sqrt(f)+g)/(sqrt(g)+f)
+    return s3
+print(s1(13,7)+s2(15,12)+s3(21,32))
