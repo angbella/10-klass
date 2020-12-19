d9d3bc4525b6ed68fdbc2272b72e6328f3985284
angbella <71321858+angbella@users.noreply.github.com>
2020-12-19 13:47:48 +0700

    Update B.py

--- Массивы/B.py
+++ Массивы/B.py
@@ -1,10 +1,13 @@
 from random import randint
-print ("Массив:")
-a=[randint(0,100) for i in range (5)]
-print (a)
-s1=[ i for i in a if i<50]
-s2=[ i for i in a if i>=50]
-sr1=sum(s1)/len(s1)
-sr2=sum(s2)/len(s2)
-print ("ср. арифм. элементов [0,50):",sr1)
-print ("ср. арифм. элементов [50,100):",sr2)
+a=[ ]
+a=[randint (0,100) for x range (5)]
+sum1=0
+sum 2=0
+for x in a:
+    if x<50:
+        sum1+=x
+        sred1=sum1//5
+    else:
+        sum2+=x
+        sred2=sum2//5
+print(sred1,sred2)
